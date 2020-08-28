1d9235e1575f84e78489cbb39afc8ec1d8a2fc4c
roman_wiens <wiensroman95@gmail.com>
2020-08-28 17:03:07 +0300

    Create project

---
+++ src/components/Card.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="card">
+    <span>Сказочное заморское явство</span>
+    <span>Нямушка</span>
+    <span>с фуа-гра</span>
+    <div class="cat"><label></label></div>
+    <div class="oval"><span>0,5</span></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card {
+  position: absolute;
+  width: 320px;
+  height: 509px;
+  left: 80px;
+  top: 104px;
+  background-color: white;
+}
+span:nth-of-type(1) {
+  position: absolute;
+  height: 19px;
+  left: 15.94%;
+  right: 20%;
+  top: calc(50% - 19px / 2 - 224px);
+
+  font-family: Trebuchet MS;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  /* identical to box height */
+
+  color: #666666;
+}
+span:nth-of-type(2) {
+  position: absolute;
+  height: 56px;
+  left: 15%;
+  right: 19.06%;
+  top: calc(50% - 56px / 2 - 181.5px);
+
+  font-family: Trebuchet MS;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 56px;
+  color: #000000;
+}
+span:nth-of-type(3) {
+  position: absolute;
+  height: 28px;
+  left: 15.94%;
+  right: 49.06%;
+  top: calc(50% - 28px / 2 - 140.5px);
+
+  font-family: Trebuchet MS;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: #000000;
+}
+.cat {
+position: absolute;
+left: -7.5%;
+right: -7.5%;
+top: 40.86%;
+bottom: -11.59%;
+background-color: white;
+}
+label {
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 0%;
+
+background: url("../assets/Photo.png");
+}
+.oval {
+position: absolute;
+left: 70%;
+right: 5%;
+top: 75.44%;
+bottom: 8.84%;
+border-radius: 100%;
+background: #1698D9;
+}
+.oval span {
+position: absolute;
+height: 52px;
+top: calc(50% - 13px);
+
+font-family: Trebuchet MS;
+font-style: normal;
+font-weight: normal;
+font-size: 42px;
+line-height: 22px;
+/* or 52% */
+
+text-align: center;
+
+color: #FFFFFF;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="page">
+      <span>Ты сегодня покормил кота?</span>
+      <card>
+        
+      </card>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Card from "@/components/Card.vue"
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Card 
   }
 };
 </script>
+
+<style scoped>
+@font-face {
+  font-family: "Exo2.0";
+  src: url("../assets/fonts/Exo2.0-Thin.otf");
+}
+.home {
+position: relative;
+width: 1280px;
+height: 680px;
+background-color: #FFFFFF;
+}
+.page {
+position: absolute;
+width: 1280px;
+height: 680px;
+left: 0px;
+top: 0px;
+background-image: url("../assets/background.png");
+}
+span {
+position: absolute;
+width: 1120px;
+height: 43px;
+left: 80px;
+top: 36px;
+font-family: "Exo2.0";
+font-style: normal;
+font-weight: 250;
+font-size: 36px;
+line-height: 43px;
+text-align: center;
+color: #FFFFFF;
+text-shadow: 0px 1px 1px #000000;
+}
+</style>>
